d2124225cef557344e713ce89f7bb789ba1d76ec
Manish Jaiswal <manish23@gmail.com>
2024-04-08 18:31:20 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <img src="https://source.unsplash.com/400x300/?car" alt="Car 1"></img>
+      <img src="https://source.unsplash.com/400x300/?auto" alt="Car 2"></img>
+      <img src="https://source.unsplash.com/400x300/?vehicle" alt="Car 3"></img>
+      <img
+        src="https://source.unsplash.com/400x300/?sportscar"
+        alt="Car 4"
+      ></img>
+      <img
+        src="https://source.unsplash.com/400x300/?luxurycar"
+        alt="Car 5"
+      ></img>
+    </div>
+  );
+}
+
+export default App;
